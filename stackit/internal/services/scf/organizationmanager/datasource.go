82b453f7880841b50b9b3f211dc91834d47619ff
akop2112 <andreas.koppenhoefer@stackit.cloud>
2025-07-30 16:23:49 +0200

    scf wip - add organization manager resource and datasource

---
+++ stackit/internal/services/scf/organizationmanager/datasource.go
@@ -0,0 +1,145 @@
+package organizationmanager
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/stackitcloud/stackit-sdk-go/core/oapierror"
+	"github.com/stackitcloud/stackit-sdk-go/services/scf"
+	"github.com/stackitcloud/terraform-provider-stackit/stackit/internal/core"
+	"github.com/stackitcloud/terraform-provider-stackit/stackit/internal/validate"
+	"net/http"
+)
+
+// Ensure the implementation satisfies the expected interfaces.
+var (
+	_ datasource.DataSource = &scfOrganizationManagerDataSource{}
+)
+
+// NewScfOrganizationManagerDataSource creates a new instance of the scfOrganizationDataSource.
+func NewScfOrganizationManagerDataSource() datasource.DataSource {
+	return &scfOrganizationManagerDataSource{}
+}
+
+// scfOrganizationManagerDataSource is the datasource implementation.
+type scfOrganizationManagerDataSource struct {
+	client       *scf.APIClient
+	providerData core.ProviderData
+}
+
+func (s scfOrganizationManagerDataSource) Metadata(ctx context.Context, request datasource.MetadataRequest, response *datasource.MetadataResponse) {
+	response.TypeName = request.ProviderTypeName + "_scf_organization_manager"
+}
+
+func (s scfOrganizationManagerDataSource) Schema(ctx context.Context, request datasource.SchemaRequest, response *datasource.SchemaResponse) {
+	response.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Description: descriptions["id"],
+				Computed:    true,
+			},
+			"region": schema.StringAttribute{
+				Description: descriptions["region"],
+				Computed:    true,
+			},
+			"platform_id": schema.StringAttribute{
+				Description: descriptions["platform_id"],
+				Required:    false,
+				Validators: []validator.String{
+					validate.UUID(),
+					validate.NoSeparator(),
+				},
+			},
+			"project_id": schema.StringAttribute{
+				Description: descriptions["project_id"],
+				Required:    true,
+				Validators: []validator.String{
+					validate.UUID(),
+					validate.NoSeparator(),
+				},
+			},
+			"org_id": schema.StringAttribute{
+				Description: descriptions["org_id"],
+				Computed:    true,
+				Validators: []validator.String{
+					validate.UUID(),
+					validate.NoSeparator(),
+				},
+			},
+			"user_id": schema.StringAttribute{
+				Description: descriptions["user_id"],
+				Computed:    true,
+				Validators: []validator.String{
+					validate.UUID(),
+					validate.NoSeparator(),
+				},
+			},
+			"username": schema.StringAttribute{
+				Description: descriptions["username"],
+				Computed:    true,
+				Validators: []validator.String{
+					stringvalidator.LengthBetween(1, 255),
+				},
+			},
+			"password": schema.StringAttribute{
+				Description: descriptions["password"],
+				Computed:    true,
+				Validators: []validator.String{
+					stringvalidator.LengthBetween(1, 255),
+				},
+			},
+			"created_at": schema.StringAttribute{
+				Description: descriptions["created_at"],
+				Computed:    true,
+			},
+			"updated_at": schema.StringAttribute{
+				Description: descriptions["updated_at"],
+				Computed:    true,
+			},
+		},
+		Description: "STACKIT Cloud Foundry organization manager datasource schema. Must have a `region` specified in the provider configuration.",
+	}
+}
+
+func (s scfOrganizationManagerDataSource) Read(ctx context.Context, request datasource.ReadRequest, response *datasource.ReadResponse) {
+	// Retrieve the current state of the resource.
+	var model Model
+	diags := request.Config.Get(ctx, &model)
+	response.Diagnostics.Append(diags...)
+	if response.Diagnostics.HasError() {
+		return
+	}
+
+	// Extract the project ID and instance id of the model
+	projectId := model.ProjectId.ValueString()
+	orgId := model.OrgId.ValueString()
+
+	// Read the current scf organization manager via orgId
+	ScfOrgManager, err := s.client.GetOrgManagerExecute(ctx, projectId, s.providerData.GetRegion(), orgId)
+	if err != nil {
+		var oapiErr *oapierror.GenericOpenAPIError
+		ok := errors.As(err, &oapiErr)
+		if ok && oapiErr.StatusCode == http.StatusNotFound {
+			response.State.RemoveResource(ctx)
+			return
+		}
+		core.LogAndAddError(ctx, &response.Diagnostics, "Error reading scf organization manager", fmt.Sprintf("Calling API: %v", err))
+		return
+	}
+
+	err = mapFieldsUpdate(ctx, ScfOrgManager, &model)
+	if err != nil {
+		core.LogAndAddError(ctx, &response.Diagnostics, "Error reading scf organization manager", fmt.Sprintf("Processing API response: %v", err))
+		return
+	}
+
+	// Set the updated state.
+	diags = response.State.Set(ctx, &model)
+	response.Diagnostics.Append(diags...)
+	tflog.Info(ctx, fmt.Sprintf("read scf organization manager %s", orgId))
+}
